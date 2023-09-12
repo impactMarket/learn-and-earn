4e54e4bd3df71e931204a3ee1b86840d1f9f651d
Bernardo Vieira <bernardo@impactmarket.com>
2023-09-12 23:36:44 +0100

    prevent access from browsers (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import Level from './components/Level/Level';
 import { DataProvider } from './context/DataContext';
 import Lesson from './components/Lesson/Lesson';
 import {
-	WagmiConfig,
-	useAccount,
-	useConnect,
-	// useNetwork,
-	// useWalletClient
+    WagmiConfig,
+    useAccount,
+    useConnect,
+    // useNetwork,
+    // useWalletClient
 } from "wagmi";
 // import { ImpactProvider } from "@impact-market/utils/ImpactProvider";
 import { chains, wagmiConfig } from './helpers/network';
@@ -22,12 +22,12 @@ import { useEffect, useState } from 'react';
 
 function Wrapper() {
     const [token, setToken] = useState('');
-	const { address, isConnected } = useAccount();
-	// const { data: signer } = useWalletClient();
-	// const { chain } = useNetwork();
-	const { connect } = useConnect({
-		connector: new InjectedConnector({ chains }),
-	});
+    const { address, isConnected } = useAccount();
+    // const { data: signer } = useWalletClient();
+    // const { chain } = useNetwork();
+    const { connect } = useConnect({
+        connector: new InjectedConnector({ chains }),
+    });
 
     useEffect(() => {
         console.log({ isConnected })
@@ -35,49 +35,57 @@ function Wrapper() {
     }, []);
     useEffect(() => {
         console.log({ address })
-		if (address) {
-			const requestOptions = {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({ address }),
+        if (address) {
+            const requestOptions = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ address }),
                 'client-id': 2,
-			};
-			fetch(
+            };
+            fetch(
 				import.meta.env.VITE_API_URL + "/users",
 				requestOptions
 			)
-				.then((response) => response.json())
-				.then((data) => setToken(data.data.token));
-		}
-	}, [address]);
+                .then((response) => response.json())
+                .then((data) => setToken(data.data.token));
+        }
+    }, [address]);
 
-	return (
-		// <ImpactProvider
-		// 	jsonRpc={import.meta.env.VITE_JSON_RPC}
-		// 	signer={signer ?? null}
-		// 	address={address ?? null}
-		// 	networkId={chain?.id || 42220}
-		// >
-			<BrowserRouter>
-                <Header />
-                <DataProvider token={token}>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/:levelId/:uid" element={<Lesson />} />
-                            <Route path="/:levelId" element={<Level />} />
-                        </Routes>
-                </DataProvider>
-            </BrowserRouter>
-		// </ImpactProvider>
-	);
+    return (
+        // <ImpactProvider
+        // 	jsonRpc={import.meta.env.VITE_JSON_RPC}
+        // 	signer={signer ?? null}
+        // 	address={address ?? null}
+        // 	networkId={chain?.id || 42220}
+        // >
+        <BrowserRouter>
+            <Header />
+            <DataProvider token={token}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/:levelId/:uid" element={<Lesson />} />
+                    <Route path="/:levelId" element={<Level />} />
+                </Routes>
+            </DataProvider>
+        </BrowserRouter>
+        // </ImpactProvider>
+    );
 }
 
 function App() {
-	return (
-        <WagmiConfig config={wagmiConfig}>
-            <Wrapper />
-        </WagmiConfig>
-	);
+    if (
+        (window as any).ethereum.isMinyPay ||
+        ((window as any).ethereum.isMetaMask &&
+            import.meta.env.VITE_ENABLE_IN_BROWSER === 'true')
+    ) {
+        return (
+            <WagmiConfig config={wagmiConfig}>
+                <Wrapper />
+            </WagmiConfig>
+        );
+    }
+
+    return null;
 }
 
 export default App;
